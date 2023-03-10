4fc5a3156973791ef3dd703383937a5c26b856b2
flydecisive <flydecisive@yandex.ru>
2023-03-11 00:13:57 +0300

    fixed stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-rational-order',
-    'stylelint-prettier/recommended',
-    'stylelint-config-standard-scss',
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+    "stylelint-prettier/recommended",
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier",
   ],
-  plugins: ['stylelint-order', 'stylelint-scss'],
+  plugins: ["stylelint-order", "stylelint-scss"],
   rules: {
-    // Дополнительные правила
+    "declaration-colon-space-after": "always",
   },
 };
